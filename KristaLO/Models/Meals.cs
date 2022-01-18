26719ef23082d3e8ec61fdf5c5cfdca392e939d9
kayris06 <93269396+kayris06@users.noreply.github.com>
2022-01-18 14:15:48 -0600

    new meal Views

--- KristaLO/Models/Meals.cs
+++ KristaLO/Models/Meals.cs
@@ -8,6 +8,7 @@ namespace FinalLiftOff.Models
     {
         public int Id { get; set; }
         public int MealID { get; set; }
+        public int Price { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Restrictions { get; set; }
