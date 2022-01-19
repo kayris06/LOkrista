5c97c8b614af4ef484389aacc6b9acfc2b762d0e
kayris06 <93269396+kayris06@users.noreply.github.com>
2022-01-19 15:06:42 -0600

    saveall from last commit

--- KristaLO/Models/Meals.cs
+++ KristaLO/Models/Meals.cs
@@ -6,18 +6,33 @@ namespace FinalLiftOff.Models
 {
     public class Meals
     {
-        public int Id { get; set; }
+        public int Id { get; }
+        static private int nextId = 1;
+
         public int MealID { get; set; }
+
         public int Price { get; set; }
+
         public string Name { get; set; }
+
         public string Description { get; set; }
+
         public string Restrictions { get; set; }
 
         
 
+        public Meals(int price, string name, string description)
+        {
+            Price = price;
+            Name = name;
+            Description = description;
+
+            Id = nextId;
+            nextId++;
+        }
+
         public Meals()
         {
         }
-       
     }
 }
